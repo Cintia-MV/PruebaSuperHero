4926f92322a351fa1a03125b3d20a9bc7b122f58
Cintia Munoz Vales <cintiamuva@gmail.com>
2022-03-09 15:53:51 -0300

    Avance tarjeta

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,44 +18,48 @@ $(document).ready(() => {
                 let peso = superHero.appearance.weight;
                 let alianzas = superHero.biography.aliases;
 
-                console.log(superHero);
-                crearGrafico(superHero);
-                for (let i = 0; i < superHero.powerstats.length; i++) {
-                    console.log(`${pokemon.stats[i].stat.name} ${pokemon.stats[i].base_stat}`);
-                }
+                let regex = new RegExp('\d');
+               /*  if (regex.test(valueInput)) { */
+                    console.log(superHero);
+                    crearGrafico(superHero);
+                    for (let i = 0; i < superHero.powerstats.length; i++) {
+                    }
 
-                $('#nombreHero').html(`
-                <div class="card mb-3" style="max-width: 540px;">
-                <div class="row no-gutters">
-                <div class="col-md-4">
-                     <img src="${imagen}">
-                </div>
-                <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Nombre: ${nombre}</h5>
-                  <p class="card-text">Conexiones: ${conexiones}</p>
-                  <p class="card-text">Publicado por: ${biografia}</p>
-                  <p class="card-text">Ocupación: ${ocupacion}</p>
-                  <p class="card-text">Primera aparición: ${primeraAparicion}</p>
-                  <p class="card-text">Altura: ${altura[0]} - ${altura[1]}</p>
-                  <p class="card-text">Peso: ${peso[0]} - ${peso[1]}</p>
-                  <p class="card-text">Alianzas: ${alianzas.join(", ")}</p>
-                </div>
-              </div>
+                    $('#nombreHero').html(`
+                 <div class="card">
+                    <div class="row no-gutters">
+                        <div class="col-md-2">
+                            <img src="${imagen}" class="card-img-left" height="50%">
+                        </div>
+                        <div class="col-md-10" style="font-family: comic neue;">
+                            <div class="card-body">
+                                <h5 class="card-title">Nombre: ${nombre}</h5>
+                                <p class="card-text">Conexiones: ${conexiones}</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Publicado por: ${biografia}</li>
+                                <li class="list-group-item">${ocupacion}</li>
+                                <li class="list-group-item">Primera aparición: ${primeraAparicion}</li>
+                                <li class="list-group-item">Altura: ${altura[0]} - ${altura[1]}</li>
+                                <li class="list-group-item">Peso: ${peso[0]} - ${peso[1]}</li>
+                                <li class="list-group-item">Alianzas: ${alianzas.join(", ")}</li>
+                            </ul>
+                        </div>
+                    </div>
                  </div>
-                </div>
-
                 `)
-
-
-
-
+                /* } else {
+                    alert('Sólo se aceptan números')
+                }; */
             }
         });
 
 
     });
 });
+
+
+
 
 const crearGrafico = (heroRecibido) => {
     let chart = new CanvasJS.Chart("chartContainer", {
@@ -64,34 +68,25 @@ const crearGrafico = (heroRecibido) => {
         title: {
             text: `Estadisticas de Poder para ${heroRecibido.name}`
         },
+        legend: {
+            cursor: "pointer",
+        },
         data: [{
             type: "pie",
+
             startAngle: 240,
             yValueFormatString: "##0.00\"%\"",
             indexLabel: "{label} {y}",
+
             dataPoints: [
-                { y: heroRecibido.powerstats.combat, label: "Combat"},
-                { y: heroRecibido.powerstats.durability, label: "Durability"},
-                { y: heroRecibido.powerstats.intelligence, label: "Intelligence"},
-                { y: heroRecibido.powerstats.power, label: "Power"},
-                { y: heroRecibido.powerstats.speed, label: "Speed"},
-                { y: heroRecibido.powerstats.strength, label: "Strength"},
+                { y: heroRecibido.powerstats.combat, label: "Combat" },
+                { y: heroRecibido.powerstats.durability, label: "Durability" },
+                { y: heroRecibido.powerstats.intelligence, label: "Intelligence" },
+                { y: heroRecibido.powerstats.power, label: "Power" },
+                { y: heroRecibido.powerstats.speed, label: "Speed" },
+                { y: heroRecibido.powerstats.strength, label: "Strength" },
             ]
         }]
     });
     chart.render();
 }
-
-
-
-
-/* const btnBuscar = document.querySelector('#btnBuscar');
-const datoInput = document.querySelector('#txtId');
-btnBuscar.addEventListener('click', (event) => {
-    event.preventDefault();
-
-   
-}); */
-
-
-
