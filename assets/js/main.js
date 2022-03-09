21f7447ee5b446b2100865c28d689fcdd75448e6
Cintia Munoz Vales <cintiamuva@gmail.com>
2022-03-08 21:41:58 -0300

    Avance 1 API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,29 @@
-$.ajax({
-    type: "get",
-    url: "https://www.superheroapi.com/api.php/3525635500807579/1",
-    dataType: "json",
-    success: function (response) {
-        console.log(response)
-    }
+$(document).ready( ()=>{
+    $("form").submit((event)=>{
+        event.preventDefault();
+        let valueInput =$("#txtId").val();
+
+         $.ajax({
+        type: "get",
+        url: `https://www.superheroapi.com/api.php/3525635500807579/${valueInput}`,
+        dataType: "json",
+        success: function (superHero) {
+            console.log(superHero)
+
+        }
+    });
+
+
+    });
 });
+
+/* const btnBuscar = document.querySelector('#btnBuscar');
+const datoInput = document.querySelector('#txtId');
+btnBuscar.addEventListener('click', (event) => {
+    event.preventDefault();
+
+   
+}); */
+
+
+
